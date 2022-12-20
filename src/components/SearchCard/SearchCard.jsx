3049a9a623cadbc07ddfcd2350ea90e2dfba8ea3
Pasha127 <pasha.account@gmail.com>
2022-12-20 15:07:01 +0100

    Search card name from URL

--- src/components/SearchCard/SearchCard.jsx
+++ src/components/SearchCard/SearchCard.jsx
@@ -6,6 +6,12 @@ import "./styles.css"
 
 
 const SearchCard = (props)=>{
+  const assetURL="https://res.cloudinary.com/dirwjcohx/image/upload/e_camera:up_20;right_-35;zoom_1;env_pillars;exposure_1.4/v1670880755/3DepotProducts/Sci-fi_Rifle_2_uykpuo.jpg"
+  let assetName= assetURL.split("/").pop().split(".").shift().split("_")
+  assetName.pop()
+  assetName= assetName.join(" ")
+
+
     const [infoClass, setInfoClass] = useState("d-none")
     const [buttonClass, setButtonClass] = useState("d-none")
     const navigate = useNavigate();
@@ -21,9 +27,9 @@ return(<>
         setInfoClass("d-none");
         setButtonClass("d-none");
       }}>
-      <Card.Img variant="top" src="https://res.cloudinary.com/dirwjcohx/image/upload/e_camera:up_20;right_-35;zoom_1;env_pillars;exposure_1.4/v1670880755/3DepotProducts/Sci-fi_Rifle_2_uykpuo.jpg" />
+      <Card.Img variant="top" src={assetURL} />
       <Card.Body className={`${infoClass}`} >
-        <Card.Title>Asset</Card.Title>
+        <Card.Title>{`${assetName}`}</Card.Title>
         <Card.Text>
           Description of Asset
         </Card.Text>
