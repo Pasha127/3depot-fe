4e83c6b7c05c4e2f6bbc7ede34b1b937c62a65e7
Pasha127 <pasha.account@gmail.com>
2022-12-30 20:50:30 +0100

    animating search choices

--- src/components/SearchCanvas/SearchCanvas.jsx
+++ src/components/SearchCanvas/SearchCanvas.jsx
@@ -1,7 +1,8 @@
 import React, { Suspense, useMemo, useState, useEffect } from 'react';
-import { Canvas, useLoader, useThree } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
 import { Environment, Html, useProgress} from '@react-three/drei';
 import { Physics, useBox, useConvexPolyhedron, useCylinder, useHeightfield, usePlane, useTrimesh } from '@react-three/cannon';
+import { animated } from "react-spring/three";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import {Mesh} from 'three'
@@ -16,8 +17,10 @@ import { useNavigate } from 'react-router-dom';
 import img from '../../assets/3DepotLogoBig.png';
 import imgAlpha from '../../assets/3DepotLogoAlpha.png';
 import crate from '../../assets/crate.png';
+import crateStencil from '../../assets/crateStencil2.png';
 import crateWall from '../../assets/warehouseBG.png';
 import floorAlpha from '../../assets/floorAlpha.png';
+import { useRef } from 'react';
 
 
 const pi= Math.PI;
@@ -27,15 +30,25 @@ const assetURL=`https://res.cloudinary.com/dirwjcohx/image/upload/e_camera:up_20
 
 THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
+const ScrollController = () =>{
+    
+}
+
+function Loader() {
+    return <Html center>
+    <Loader2D/>
+  </Html>
+}
+
 const CameraController = () => {
-  const { camera, gl } = useThree();
-  useEffect(
-    () => {
-      const controls = new OrbitControls(camera, gl.domElement);
-      controls.minDistance = .5;
+    const { camera, gl } = useThree();
+    useEffect(
+        () => {
+            const controls = new OrbitControls(camera, gl.domElement);
+            controls.minDistance = .5;
       controls.maxDistance = 20;
-      controls.enableZoom = false;
-      controls.enableRotate = false;
+/*       controls.enableZoom = false;
+      controls.enableRotate = false; */
 
       
       return () => {
@@ -47,9 +60,7 @@ const CameraController = () => {
   return null;
 };
 
-const ScrollController = () =>{
 
-}
 
 function ObjToPrimitive({ url, mat }) {
   const [obj, setObj] = useState();
@@ -96,15 +107,14 @@ function FBXAsset() {
     
 }
 
-function Loader() {
-  const {progress} = useProgress()
-  return <Html center>
-    <Loader2D/>
-  </Html>
-}
 
 function Box(props) {
+    const [clicked,setClicked] = useState(false);
+    const [canClick,setCanClick] = useState(true);
+    const navigate = useNavigate();
+    const goToGarage = () => navigate('/Garage');
     const texture = new THREE.TextureLoader().load( crate );
+    const textureStencil = new THREE.TextureLoader().load( crateStencil );
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set( 1, 1);
@@ -121,42 +131,46 @@ function Box(props) {
       rotation:[-pi/3,-pi/3,0]
     }));
     
-    return(
-  <mesh onClick={()=>{
-    const raycaster = new THREE.Raycaster();
+    return(<>
+  <mesh onClick={(e)=>{
+    e.stopPropagation()
+    /* const raycaster = new THREE.Raycaster();
     const vector = new THREE.Vector3( 0, 0, 0 ); // instead of event.client.x and event.client.y
     const direction = new THREE.Vector3( 0, 0, -1 ).transformDirection( camera.matrixWorld );
     raycaster.set( vector, direction );
-    const intersects = raycaster.intersectObjects(objects);
-    if(props.canClick){
-        props.setCanClick(false);
-        setTimeout(()=>props.setCanClick(true),2000)
-        if(!props.clicked){
+    const intersects = raycaster.intersectObjects(objects); */
+    if(canClick){
+        setCanClick(false);
+        setTimeout(()=>setCanClick(true),2000)
+        if(!clicked){
     api.velocity.set(0,3,3);
     api.applyTorque([4,10,1])
-    props.setClicked(true)}
+    setClicked(true)}
     else{
         api.velocity.set(0,3,-3);
         api.applyTorque([-4,-10,-1])
-        props.setClicked(false)}
+        setClicked(false)}
     }}
 }
   
   ref={ref} >
     <boxBufferGeometry attach="geometry" args={[1,1]}/>
-    <meshStandardMaterial attatch="material" map={texture}/>
-    <mesh rotation={[pi/2,0,pi]} position={[0,-.51,0]}>
+    <meshStandardMaterial attatch="material" map={texture} transparent/>
+    <mesh rotation={[pi/2,0,pi]} position={[0,-.501,0]}>
         <planeBufferGeometry attach="geometry" args={[1,1]}/>
         <meshStandardMaterial attach="material" map={texture2} transparent/>
     </mesh>
-    <mesh rotation={[pi/2,pi,0]} position={[0,.51,0]}>
+    <mesh rotation={[pi/2,pi,0]} position={[0,.501,0]} onClick={(e)=>{
+        /* e.stopPropagation()
+        goToGarage(); */
+    }}>
         <planeBufferGeometry attach="geometry" args={[1,1]}/>
-        <meshStandardMaterial attach="material" map={texture2} transparent/>
+        <meshStandardMaterial attach="material" map={texture2} color="black" transparent/>
     </mesh>
   </mesh>
 
   
-    );
+    </>);
   }
 
 
@@ -178,22 +192,31 @@ function RearPlane() {
     const textureWC = new THREE.TextureLoader().load( crateWall );
   return(
 <mesh position={[0,-.2,-1]} rotation={[0,0,0]}>
-  <planeBufferGeometry  attach="geometry" args={[18,9,200]}/>
+  <planeBufferGeometry  attach="geometry" args={[19,9,200]}/>
   <meshLambertMaterial attach="material"  map={textureWC} />
 </mesh>
   );
 }
 
-function Image() {
-  const texture = useLoader(THREE.TextureLoader, img)
-  const textureAlpha = useLoader(THREE.TextureLoader, imgAlpha)
-  return (
-    <mesh position={[0,-10,0]} rotation={[-pi/2,0,pi/2]}>
-      <planeBufferGeometry attach="geometry" args={[30, 30]} />
-      <meshStandardMaterial attach="material" color={"#cccccc"} alphaMap={textureAlpha} transparent/>
-    </mesh>
-  )
+
+function PreviewAsset(){
+const rotatingMesh = useRef();
+const [active, setActive] = useState(false);
+
+useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    rotatingMesh.current.rotation.y = elapsedTime;
+  });
+
+    return(<>
+        {active && <animated.mesh ref={rotatingMesh} position={[.05,.5,1.73]} rotation={[0,0,0]}>
+          <boxBufferGeometry  attach="geometry" args={[1,1,1]}/>
+          <meshLambertMaterial attach="material" wireframe={true} />
+          <FBXAsset/>
+        </animated.mesh>}
+    </>);
 }
+
 
 const mapStateToProps = state => {
   return {
@@ -212,8 +235,7 @@ const mapStateToProps = state => {
 
 
 function SearchCanvas(props) {
-    const [clicked,setClicked] = useState(false);
-    const [canClick,setCanClick] = useState(true);
+    const [oneActive,setOneActive] = useState(false);
   const navigate = useNavigate();
   const goToLogIn = () => navigate('/LogIn');
   
@@ -227,21 +249,18 @@ function SearchCanvas(props) {
       <Canvas>
         <Suspense fallback={<Loader/>}>
         <CameraController/>
-        {/* <Environment background={true} files={'industrial_workshop_foundry_bw2.hdr'}
-              path={'/'} ></Environment> */}
-        <Image/>
         <Physics>
         <RearPlane/>
         <FloorPlane/>
-        <Box xPos={1.5} setClicked={setClicked} clicked={clicked} canClick={canClick} setCanClick={setCanClick}/>
-        <Box xPos={3.5} setClicked={setClicked} clicked={clicked} canClick={canClick} setCanClick={setCanClick}/>
-        <Box xPos={5.5} setClicked={setClicked} clicked={clicked} canClick={canClick} setCanClick={setCanClick}/>
+        <Box key={1.5} xPos={1.5}/>
+        <Box key={3.5} xPos={3.5}/>
+        <Box key={5.5} xPos={5.5}/>
         </Physics>
+        <PreviewAsset/>
         <ambientLight intensity={.3}/>
         <spotLight position={[200,800,500]} angle={0.3} color={`rgb(${props.settings.red},${props.settings.green},${props.settings.blue})`} intensity={props.settings.intensity}/>
         <spotLight position={[-600,800,500]} angle={0.3} color={`rgb(${props.settings.red},${props.settings.green},${props.settings.blue})`} intensity={props.settings.intensity}/>
         <primitive object={new THREE.AxesHelper(props.settings.axesSize)}></primitive>
-
         </Suspense>
       </Canvas>
     </div>
